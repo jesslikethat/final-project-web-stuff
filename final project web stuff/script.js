0500b721fd2a14ab73d27265f42070269a6216d0
jesslikethat <30765295+jesslikethat@users.noreply.github.com>
2019-05-09 23:06:14 -0700

    Add files via upload

---
+++ final project web stuff/script.js
@@ -0,0 +1,76 @@
+$(".Metacritic").on("click", function() {
+    $(".Oscar").css("background", "#f1c345");
+    $(".MetacriticNum").show();
+})
+
+$(".WorldwideBoxOffice").on("click", function() {
+    $(".Oscar").css("background", "#f3cd4f");
+    $(".WorldwideBoxOfficeNum").show();
+})
+
+$(".IMDB").on("click", function() {
+    $(".Oscar").css("background", "#f4d261");
+    $(".IMDBNum").show();
+})
+
+
+$(".RottenTomatoes").on("click", function() {
+    $(".Oscar").css("background", "#f3d364");
+    $(".RottenTomatoesNum").show();
+})
+
+$(".Rank").on("click", function() {
+    $(".Oscar").css("background", "#f5d56a");
+    $(".RankNum").show();
+})
+
+$(".Director1").on("click", function() {
+    $(".Oscar").css("background", "#f5d66f");
+    $(".Director1Num").show();
+})
+
+
+$(".Actors_3").on("click", function() {
+    $(".Oscar").css("background", "#f5dc85");
+    $(".Actors_3Num").show();
+})
+
+$(".Actors_2").on("click", function() {
+    $(".Oscar").css("background", "#f7df8b");
+    $(".Actors_2Num").show();
+})
+$(".Genre2").on("click", function() {
+    $(".Oscar").css("background", "#f6de8b");
+    $(".Genre2Num").show();
+})
+$(".Actors_1").on("click", function() {
+    $(".Oscar").css("background", "#f8e49a");
+    $(".Actors_1Num").show();
+})
+$(".Genre_1").on("click", function() {
+    $(".Oscar").css("background", "#f8e8a6");
+    $(".Genre_1Num").show();
+})
+
+$(".Language_1").on("click", function() {
+    $(".Oscar").css("background", "re#f8e8a7d");
+    $(".Language_1Num").show();
+})
+
+$(".Language_2").on("click", function() {
+    $(".Oscar").css("background", "#f9e9aa");
+    $(".Language_2Num").show();
+})
+$(".Country").on("click", function() {
+    $(".Oscar").css("background", "#faedb5");
+    $(".CountryNum").show();
+})
+$(".director_2").on("click", function() {
+    $(".Oscar").css("background", "#faf2c6");
+    $(".director_2Num").show();
+})
+
+$(".language_1").on("click", function() {
+    $(".Oscar").css("background", "#faf3c8");
+    $(".OscarNum").show();
+})
